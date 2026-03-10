ef5e3521ac19d45de5f1e255b09d75c1b21e6bb4
Lauraisabelcg <isateriyaki04@gmail.com>
2026-03-10 12:31:55 -0500

    Commit: se ha agregado precio a cada cafe

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ def menu():
 		1.Cafe con leche
 		2.Cafe americano
 		3.Cafe descafeinado""")
-
+	match menu:
+		case 1:
+			print("Bienvenido el cafe con leche tiene un valor de 2000")
+		case 2:
+			print("Bienvenido el cafe ameriano tiene un valor de 3000")
+		case 3:
+			print("Bienvenido el cafe descafeinado tiene un valor de 5000")

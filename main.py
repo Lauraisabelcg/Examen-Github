bdaafa5e01c6325b38c978c649555429d5021564
Lauraisabelcg <isateriyaki04@gmail.com>
2026-03-10 12:39:36 -0500

    Primer commit:Editado el precio del cafe con leche

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def menu():
 		3.Cafe descafeinado""")
 	match menu:
 		case 1:
-			print("Bienvenido el cafe con leche tiene un valor de 2000")
+			print("Bienvenido el cafe con leche tiene un valor de 1500")
 		case 2:
 			print("Bienvenido el cafe ameriano tiene un valor de 3000")
 		case 3:
